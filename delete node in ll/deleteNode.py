4381b76460fb99ae4de37dfb618675ca3855ec60
eslam <esla.ahme@hotmail.com>
2020-06-04 10:58:34 +0200

    Better algoritm

--- delete node in ll/deleteNode.py
+++ delete node in ll/deleteNode.py
@@ -1,10 +1,4 @@
 class Solution(object):
     def deleteNode(self, node):
-        while node.next != None:
             node.val = node.next.val
-            
-            if node.next.next == None:
-                node.next = None
-            else:
-                node = node.next
-        
+            node.next = node.next.next

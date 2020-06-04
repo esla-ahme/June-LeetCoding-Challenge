b77a714d7ae6df695103e64f5c60b3ec500d2b34
eslam <esla.ahme@hotmail.com>
2020-06-04 11:05:59 +0200

    Day2 better approach

--- delete node in ll/deleteNode.cpp
+++ delete node in ll/deleteNode.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        while (node->next != nullptr)
-        {
-            node->val = node->next->val;
-            
-            if (node->next->next == nullptr)
-                node->next = nullptr;
-            else
-                node = node->next; 
-        }      
+        node->val = node->next->val;    
+    	node->next = node->next->next;
     }
 };
--- delete node in ll/deleteNode.py
+++ delete node in ll/deleteNode.py
@@ -2,9 +2,5 @@ class Solution(object):
     def deleteNode(self, node):
         while node.next != None:
             node.val = node.next.val
-            
-            if node.next.next == None:
-                node.next = None
-            else:
-                node = node.next
+            node.next = node.next.next 
         

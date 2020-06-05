d0d330d6a8127a3d84db8ca4cfc06fbfe57d4f04
eslam <esla.ahme@hotmail.com>
2020-06-05 11:12:56 +0200

    Accepted

--- RandomWithWeithWeights/weightedRandom.cpp
+++ RandomWithWeithWeights/weightedRandom.cpp
@@ -1,25 +1,34 @@
-/*
-Date : 10:46AM 6/6/2020
-Message : this code caused A time limit exceeded error because it is O(n^2) array length*weight
-
-*/
 class Solution {
 public:
     vector<int> v;
+    /*
+        if w = [1,2,3]
+        v will be [1,3,6]
+    */
     Solution(vector<int>& w) {
-        int i =0;
-        for (int weight : w){
-            while(weight--)
-            {
-                v.push_back(i);
-            }
-            i++;
-        }
+        //get perfix sum
+        v.resize(w.size());
+        v[0] = w[0];
+        for (int i = 1 ; i < w.size() ; i++)
+            v[i] = v[i-1] + w[i];
+        
     }
     
+    /*
+     get random number from 0 : 5 
+     0 -> 1 with index 0
+     1,2 -> 3 which index is 1
+     3,4,5 -> 6 which index is 2 
+    */
+    
     int pickIndex() {
-        return v[rand()%v.size()];
+        // get ranom number in range of the psum array
+        int rNum = rand()%v.back();
+        // get iterator points to the location of the picked number
+        //upper_bound function handle this mapping
+        auto it = upper_bound(v.begin(),v.end(),rNum);
+        // get the index of the picked number 
+        int index = it - v.begin();
+        return index;
     }
 };
-
-

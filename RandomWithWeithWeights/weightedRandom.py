e470beb334ae0ea40207a9f4255c8346ff038d02
eslam <esla.ahme@hotmail.com>
2020-06-05 11:54:32 +0200

    Day 5

--- RandomWithWeithWeights/weightedRandom.py
+++ RandomWithWeithWeights/weightedRandom.py
@@ -5,7 +5,6 @@ class Solution:
             w[i] += w[i-1]
         self.v = w
         
-    
 
     def pickIndex(self) -> int:     
         return bisect_left(self.v, random.random()*self.v[-1])
